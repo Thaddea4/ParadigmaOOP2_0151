09c3636c5b819616ea157be357e3fe1712364e7c
Thaddea4 <adeyasyie@gmail.com>
2026-05-18 20:13:54 +0700

    menambahkan class rekeningpremium dan program utama

--- tugas.cpp
+++ tugas.cpp
@@ -25,3 +25,44 @@ public:
              << biayaAdmin << endl;
     }
 };
+class RekeningPremium : public RekeningBank {
+private:
+    int saldoAkhir;
+
+public:
+    RekeningPremium(int saldo) : saldoAkhir(saldo) {
+
+    }
+
+    void potongAdmin() {
+        if (saldoAkhir > 10000000) {
+            cout << "Rekening Premium bebas biaya admin" << endl;
+        }
+        else {
+            cout << "Rekening Premium terkena biaya admin Rp 50000" << endl;
+        }
+    }
+};
+
+int main() {
+
+    RekeningBank* rekening1;
+    RekeningBank* rekening2;
+    RekeningBank* rekening3;
+
+    rekening1 = new RekeningSyariah();
+    rekening2 = new RekeningKonvensional();
+    rekening3 = new RekeningPremium(8000000);
+
+    rekening1->potongAdmin();
+
+    cout << "====================" << endl;
+
+    rekening2->potongAdmin();
+
+    cout << "====================" << endl;
+
+    rekening3->potongAdmin();
+
+    return 0;
+}

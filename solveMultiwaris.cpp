431c7644552fc6144f9bb540f484a22393ce26d4
Thaddea4 <adeyasyie@gmail.com>
2026-05-18 18:15:51 +0700

    menambahkan class pelajar warisan dari orang

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -20,3 +20,12 @@ public:
         cout << "pekerja dibuat\n" << endl;
     }
 };
+
+class pelajar : public orang {
+public:
+    pelajar(int pUmur) :
+    orang(pUmur)
+    {
+        cout << "pelajar dibuar\n" << endl;
+    }
+};

a9d8352e3c3c3c88fb34e85270d2a613cc892ff5
Thaddea4 <adeyasyie@gmail.com>
2026-05-18 15:18:08 +0700

    menambahkan method untuk mengisi data

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -5,3 +5,9 @@ class AbtraksiKlas {
 private: string x, y;
 
 public:
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
